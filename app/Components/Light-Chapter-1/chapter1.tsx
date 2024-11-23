093003fe00fea584666b4a2d5e16ac1b9965a22e
AravinthanN <aravinthankpt@gmail.com>
2024-11-23 21:54:38 +0530

    vhyv

--- app/Components/Light-Chapter-1/chapter1.tsx
+++ app/Components/Light-Chapter-1/chapter1.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useInView } from "react-intersection-observer";
+
 import PopUp from "@/app/PopOverComponent/PopOverComponent";
 import React from "react";
 import texts from "./textFile";
@@ -7,30 +7,12 @@ import texts from "./textFile";
 const TextWithPopover = () => {
   return (
     <div className="max-w-[800px] min-h-screen mx-auto mb-10 mt-5 bg-[#fffaf1]">
-      {texts.map((text, index) => {
-        // Use the hook correctly in the component scope
-        const [ref, inView] = useInView({
-          threshold: 0.2, // Trigger animation when 20% of the component is in view
-        });
-
-        return (
-          <div
-            ref={ref} // Apply ref here
-            key={index}
-            className={`w-full h-auto mx-2 transition-transform duration-1000 ${
-              inView
-                ? index % 2 === 0
-                  ? "translate-x-0 opacity-100"
-                  : "translate-x-0 opacity-100"
-                : index % 2 === 0
-                ? "-translate-x-full opacity-0"
-                : "translate-x-full opacity-0"
-            }`}
-          >
-            <PopUp content={text} />
-          </div>
-        );
-      })}
+      {texts.map((text, index) => (
+        <div key={index} className="w-[100%] h-auto mx-2">
+          {/* Render content depending on whether it has HTML */}
+          <PopUp content={text} />
+        </div>
+      ))}
     </div>
   );
 };

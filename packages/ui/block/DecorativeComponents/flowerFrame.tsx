3fd20d2da577df078d214d13b2f3b8f92d078563
AravinthanN <aravinthankpt@gmail.com>
2025-08-03 14:39:54 +0530

    fix:code update

--- packages/ui/block/DecorativeComponents/flowerFrame.tsx
+++ packages/ui/block/DecorativeComponents/flowerFrame.tsx
@@ -4,10 +4,10 @@ import React, { ReactNode } from 'react';
 
 const FloralFrame = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="relative w-full h-screen p-8 bg-[#fffaf1] overflow-hidden rounded-3xl shadow-xl">
+    <div className="relative w-full h-screen p-8 bg-[#fffaf1] overflow-hidden  shadow-xl">
       {/* Border Image */}
       <img
-        className="absolute inset-0 w-full h-full  pointer-events-none select-none z-10 scale-y-[1.12]"
+        className="absolute inset-0 w-full h-full  pointer-events-none select-none z-10 scale-y-[1.11] scale-x-[1.08]"
         src="/images/borderflowers1.png"
         alt="Floral Border Frame"
       />
